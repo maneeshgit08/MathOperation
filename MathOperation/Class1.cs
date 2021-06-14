f05d7964c0ff6fd088f72287dfb6650c8461b2a3
Kabra, Manish (CT RDA DS AA DTS AU APPS) <manish.kabra@siemens.com>
2021-06-14 15:49:39 +0530

    first commit

---
+++ MathOperation/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation
+{
+    public class Class1
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
